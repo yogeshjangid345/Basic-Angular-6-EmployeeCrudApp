f5625ae800df11e7591475863c46404922458d89
Vineeta Sharma <vineetasharma18791gmail.com>
2019-04-28 15:08:54 +0530

    basic setup angular 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'crud-employee';
+  title = 'Employee Details';
+
+firstName = 'Jai guru';
+
 }
